cf29ee87231ec045c125cbdc61b69974ef0ec819
EloiGry <eloi.grychta@gmail.com>
2022-01-13 17:32:37 +0100

    homePage design en cours

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import HomePage from './pages/HomePage';
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>} />
+        <Route exact path="/" element={<Login/>} />
+        <Route exact path="/homePage" element={<HomePage/>} />
       </Routes>
     </BrowserRouter>
   );
