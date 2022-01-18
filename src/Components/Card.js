6a2d6f790cf7d6bc3ef5ef2550ef9c63d32204ba
Nascimento95 <marco-pereira@hotmail.fr>
2022-01-18 10:15:46 +0100

    erreur resolu

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,31 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import  {Link}  from 'react-router-dom'
 
 const Card = () => {
     return (
       <div className="card">
         <img className="card-img-top" style={{height:'210px',width:'100%'}} src="https://img-19.ccm2.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg"  alt="picture_profile"/>
         <div className="card-body">
-        <button >Edit profile</button>
-          <h5 className="card-title">Coma Nas</h5>
-          <h6>@Coma_nas</h6>
+            <button >Edit profile</button>
+            <h5 className="card-title">Coma Nas</h5>
+            <h6>Coma_nas</h6>
           <div className='d-flex'>
-          <img src="https://img.icons8.com/fluency-systems-regular/21/000000/calendar--v1.png" alt="icon_calendary"/>
-         Joined January 2022
+            <img src="https://img.icons8.com/fluency-systems-regular/21/000000/calendar--v1.png" alt="icon_calendary"/>
+              Joined January 2022
           </div>
-          <div className='d-flex mt-2'>
-            <Link to="/" ><p>10 Following</p></Link>
-            <Link to="/" ><p className='mx-2'>1 Followers</p></Link>
-          </div>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <div className='d-flex'>
-            <Link to="/" ><p>Tweets</p></Link>
-            <Link to="/" ><p className='mx-2'>Tweets & replies</p></Link>
-            <Link to="/" ><p>Media</p></Link>
-            <Link to="/" ><p className='mx-2'>Like</p></Link>
-          </div>
+            <div className='d-flex mt-2'>
+              <Link to="/" ><p>10 Following</p></Link>
+              <Link to="/" ><p className='mx-2'>1 Followers</p></Link>
+            </div>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div className='d-flex'>
+              <Link to="/" ><p>Tweets</p></Link>
+              <Link to="/" ><p className='mx-2'>Tweets  replies</p></Link>
+              <Link to="/" ><p>Media</p></Link>
+              <Link to="/" ><p className='mx-2'>Like</p></Link>
+            </div>
         </div>
-        
       </div>
     );
 };
