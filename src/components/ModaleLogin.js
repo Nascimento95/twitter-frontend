c6b8aab8d9e9795b06f2019aa63230187c43b0f9
Nascimento95 <marco-pereira@hotmail.fr>
2022-01-19 09:29:32 +0100

    jai changer connection dans la modal login *

--- src/components/ModaleLogin.js
+++ src/components/ModaleLogin.js
@@ -8,7 +8,7 @@ const ModaleLogin = ({showModal, closeModal }) => {
         <div>
             <Modal show={showModal} onHide={closeModal}>
                 <Modal.Header  closeButton>
-                <Modal.Title><img style={{marginLeft:"200px"}} src="https://img.icons8.com/color/48/000000/twitter--v1.png" alt="icon_twitter"/> <br/> <h3>Créer votre compte</h3></Modal.Title>
+                <Modal.Title><img style={{marginLeft:"200px"}} src="https://img.icons8.com/color/48/000000/twitter--v1.png" alt="icon_twitter"/> <br/> <h3>Connecter-vous</h3></Modal.Title>
                 </Modal.Header>
                 <Modal.Body style={{height:"400px",overflow:"scroll"}} variant='scrollable'>
                    {/* <FormSignup /> */}
