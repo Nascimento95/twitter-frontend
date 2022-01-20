d342bbd6120c9b1d6509452ecd3c9978685e5900
BilalKeb <90609831+BilalKeb@users.noreply.github.com>
2022-01-20 13:22:08 +0100

    First

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 import HomePage from './pages/HomePage';
 import UserPage from './pages/UserPage';
+import Messages from './pages/Messages';
+
+
 const App = () => {
   return (
     <BrowserRouter>
@@ -12,6 +15,7 @@ const App = () => {
         <Route exact path="/" element={<Login/>} />
         <Route exact path="/homePage" element={<HomePage/>} />
         <Route exact path="/userPage" element={<UserPage/>} />
+        <Route exact path="/messages" element={<Messages/>} />
         <Route path="*" element={<NotFound/>} />
       </Routes>
     </BrowserRouter>
